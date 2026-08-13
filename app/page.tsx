f9401850b80ebad9a1dfdf02361bd05571037878
Eleven-stack11 <erwinlimantoo@gmail.com>
2026-08-14 01:00:24 +0700

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: "40px", color: "white", background: "#0F0F0E", minHeight: "100vh" }}>
-      <h1 style={{ fontSize: "48px" }}>The Tempering</h1>
-      <p>Hello World! Build berhasil!</p>
-    </div>
-  );
-}
