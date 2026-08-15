5068b2c28ee6b617701ee5f8f493b8bcd57fec31
Eleven-stack11 <erwinlimantoo@gmail.com>
2026-08-15 12:18:52 +0700

    Update page.tsx

--- app/month/[slug]/page.tsx
+++ app/month/[slug]/page.tsx
@@ -154,7 +154,7 @@ export default async function MonthPage({ params }: { params: Promise<{ slug: st
       {/* ===== MINGGU-MINGGU ===== */}
       <section>
         <div className="sec-head">
-          <h2 className="text-3xl md:text-4xl">Minggu-Minggu</h2>
+          <h2 className="text-3xl md:text-4xl">Monthly note</h2>
           <p className="text-base md:text-lg text-[#A6A39C]">{description}</p>
         </div>
 
